233f8d297866c4786d8e770f64c0b9bf1894969a
farahmuzaffar <164481851+farahmuzaffar@users.noreply.github.com>
2024-04-22 22:41:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoQuestions = await inquirer.prompt([
+        {
+            type: "input",
+            name: "firstQuestion",
+            message: "What would you like to add in your todos?"
+        },
+        {
+            type: "confirm",
+            name: "secondQuestion",
+            message: "Would you like to add more in your todos?",
+            default: "true"
+        }
+    ]);
+    todos.push(todoQuestions.firstQuestion);
+    console.log(todos);
+    condition = todoQuestions.secondQuestion;
+}
